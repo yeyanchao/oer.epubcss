fb6b8f80b93e9239fb187aae6b356cea31d6fbbb
phil <phil@cnx.org>
2012-04-05 06:52:01 -0400

    Moved the CSS Evaluator into util so it's by the CSS Parser

--- oer/custom/util.py
+++ oer/custom/util.py
@@ -1,6 +1,17 @@
 import cssselect # The customized one
 from lxml.cssselect import TokenStream, String, Symbol, Token
 
+import numbers
+
+class State(object):
+  def __init__(self, node = None, state = None):
+    self.node = node
+    self.counters = {}
+    self.strings = {}
+    if state:
+      self.counters = state.counters.copy()
+      self.strings = state.strings.copy()
+
 def parse_style(style, class_ = ''):
   news = {}
   if class_:
@@ -183,6 +194,137 @@ class ContentPropertyParser(object):
     string_name = stream.next()
     assert str(stream.next()) == ')'
     return string_name
+
+class ContentEvaluator(object):
+  def __init__(self, node_at = {}, state = None, verbose = False):
+    self.verbose = verbose
+    self.node_at = node_at
+    self.state = State()
+    if state:
+      self.state = state
+
+  def eval_content(self, node, content):
+    vals = [] # Accumulator
+    for (function, args) in content:
+      if function is None:
+        vals.append(args)
+      else:
+        name = function
+        method = '_eval_' + name.replace('-', '_')
+        if not hasattr(self, method):
+          raise UnsupportedError("The CSS content function %r is unsupported" % name)
+        method = getattr(self, method)
+        val = method(node, args)
+        if val is not None: vals.append(val)
+
+    if self.verbose: print >> sys.stderr, "DEBUG: Generated: [%s] from content:[%s]" % (''.join(vals), content)
+    ret = ''.join(vals)
+    return ret
+
+  def lookup_state(self, node, attr):
+    id = node.attrib.get(attr, None)
+    if id:
+      if id[0] == '#':
+        id = id[1:]
+      if id in self.node_at:
+        return self.node_at[id]
+  
+  def lookup_text(self, node, attr, which):
+    """ Used by target-text(attr(href), content(first-letter)) """
+    state = self.lookup_state(node, attr)
+    if state:
+      if which == 'before':
+        if len(state.node) > 0 and self.is_pseudo(state.node[0]):
+          return state.node[0].text # guaranteed it doesn;t have child elements
+      elif which == 'after':
+        if len(state.node) > 0 and self.is_pseudo(state.node[-1]):
+          return state.node[-1].text # guaranteed it doesn;t have child elements
+      else:
+        text = ''
+        if state.node.text: text += state.node.text
+        def rec_add(n):
+          text = ''
+          if n.text: text += n.text
+          for s in n: text += rec_add(s)
+          if n.tail: text += n.tail
+          return text
+        for child in state.node:
+          if not self.is_pseudo(child):
+            text += rec_add(child)
+          elif child.tail: text += child.tail
+        text = ''.join(text)
+        if which == 'first-letter':
+          text = text.strip()
+          if text: return text[0]
+          else: return ''
+        else: return text
+    
+  def lookup_counter(self, node, attr, name):
+    # Look up the node (strip of the leading "#" in the href)
+    v = 0
+    state = self.lookup_state(node, attr)
+    if state:
+      if not state.counters:
+        if self.verbose: print >> sys.stderr, "WARNING: Trying to get target-counter of a non-existent id '%s'" % id
+      elif name in state.counters:
+        v = state.counters[name]
+    else:
+      if self.verbose: print >> sys.stderr, "WARNING: Element %s does not have attribute '%s' to look up" % (node.tag, attr)
+    return v
+
+  def _eval_target_text(self, node, args):
+    (attr, which) = args
+    n = node
+    if self.is_pseudo(node):
+      n = node.getparent()
+    v = self.lookup_text(n, attr, which)
+    return v
+
+  def _eval_target_counter(self, node, args):
+    (attr, name, numbering) = args
+    n = node
+    if self.is_pseudo(node):
+      n = node.getparent()
+    v = self.lookup_counter(n, attr, name)
+    if v and name != 'page':
+      # TODO: use numbering to customize how it's rendered (decimal, upper-roman, etc)
+      return numbers.toString(v, numbering)
+
+  def _eval_counter(self, node, args):
+    # These look like: "counter(chapter)" or "counter(chapter, upper-roman)"
+    (name, numbering) = args
+    v = 0
+    if name in self.state.counters:
+      v = self.state.counters[name]
+    if v and name != 'page':
+      return numbers.toString(v, numbering)
+
+  def _eval_attr(self, node, args):
+    (name, type_, value) = args
+    n = node
+    if self.is_pseudo(node):
+      n = node.getparent()
+    v = n.attrib.get(name, '')
+    return v
+
+  def _eval_content(self, node, args):
+    # TODO: Just the text may not be enough to match the spec
+    assert args == ''
+    return node.text
+
+  def _eval_leader(self, node, args):
+    # Ignore the leader function
+    pass
+
+  # http://www.w3.org/TR/css3-gcpm/#using-named-strings
+  def _eval_string(self, node, args):
+    print "Evaluating string and it's: %s" % str(self.state.strings)
+    if args in self.state.strings:
+      return self.state.strings[args]
+
+  def is_pseudo(self, node):
+    return node.attrib.get('class', '') in ('pseudo-before', 'pseudo-after')
+
 
 def _is_int(s):
   try: 
--- oer/epubcss.py
+++ oer/epubcss.py
@@ -6,7 +6,7 @@ from lxml import etree
 
 from custom import premailer
 from custom import numbers
-from custom.util import PropertyParser, ContentPropertyParser, parse_style
+from custom.util import PropertyParser, ContentPropertyParser, parse_style, ContentEvaluator, State
 
 __all__ = ['AddNumbering', 'UnsupportedError']
 
@@ -14,19 +14,12 @@ STYLE_ATTRIBUTE = '_custom_style'
 
 class UnsupportedError(Exception): pass
 
-class State(object):
-  def __init__(self, node, counters, strings):
-    self.node = node
-    self.counters = counters.copy()
-    self.strings = strings.copy()
-
 class AddNumbering(object):
 
   def __init__(self, pseudo_element_name='{http://www.w3.org/1999/xhtml}span', verbose=False):
-    self.counters = {}
     self.node_at = {}
+    self.evaluator = ContentEvaluator(self.node_at)
     self.reprocess = [] # nodes with content: target-counter(....) and the current counter values at that point for the node: (etree.Element, {'name', 4})
-    self.strings = {} # http://www.w3.org/TR/css3-gcpm/#named-strings
     self.verbose = verbose
     self.pseudo_element_name = pseudo_element_name
 
@@ -59,8 +52,8 @@ class AddNumbering(object):
       self.mutate_node(node)
 
     if self.verbose: print >> sys.stderr, "-------- Resolving link counters ( CSS3 target-counter ) : %d" % len(self.reprocess)
-    for (node, self.countersAt) in self.reprocess:
-      self.counters = self.countersAt
+    for (node, self.evaluator.state.countersAt) in self.reprocess:
+      self.evaluator.state.counters = self.evaluator.state.countersAt
       d = PropertyParser().parse(node.attrib.get(STYLE_ATTRIBUTE, ''))
       if 'content' in d:
         self._replace_content(node, d['content'])
@@ -89,143 +82,24 @@ class AddNumbering(object):
     # instead of the expected:
     # <node><pseudo-before>...</pseudo-before>foo</node>
     #
-    text = self.make_content(node, content)
+    text = self.evaluator.eval_content(node, content)
     if len(node) > 0 and self.is_pseudo(node[0]):
       node[0].tail = text
     else:
       node.text = text
-
-  def lookup_state(self, node, attr):
-    id = node.attrib.get(attr, None)
-    if id:
-      if id[0] == '#':
-        id = id[1:]
-      if id in self.node_at:
-        return self.node_at[id]
-  
-  def lookup_text(self, node, attr, which):
-    """ Used by target-text(attr(href), content(first-letter)) """
-    state = self.lookup_state(node, attr)
-    if state:
-      if which == 'before':
-        if len(state.node) > 0 and self.is_pseudo(state.node[0]):
-          return state.node[0].text # guaranteed it doesn;t have child elements
-      elif which == 'after':
-        if len(state.node) > 0 and self.is_pseudo(state.node[-1]):
-          return state.node[-1].text # guaranteed it doesn;t have child elements
-      else:
-        text = ''
-        if state.node.text: text += state.node.text
-        def rec_add(n):
-          text = ''
-          if n.text: text += n.text
-          for s in n: text += rec_add(s)
-          if n.tail: text += n.tail
-          return text
-        for child in state.node:
-          if not self.is_pseudo(child):
-            text += rec_add(child)
-          elif child.tail: text += child.tail
-        text = ''.join(text)
-        if which == 'first-letter':
-          text = text.strip()
-          if text: return text[0]
-          else: return ''
-        else: return text
-    
-  def lookup_counter(self, node, attr, name):
-    # Look up the node (strip of the leading "#" in the href)
-    v = 0
-    state = self.lookup_state(node, attr)
-    if state:
-      if not state.counters:
-        if self.verbose: print >> sys.stderr, "WARNING: Trying to get target-counter of a non-existent id '%s'" % id
-      elif name in state.counters:
-        v = state.counters[name]
-    else:
-      if self.verbose: print >> sys.stderr, "WARNING: Element %s does not have attribute '%s' to look up" % (node.tag, attr)
-    return v
-
-  def make_content(self, node, content):
-    vals = [] # Accumulator
-    for (function, args) in content:
-      if function is None:
-        vals.append(args)
-      else:
-        name = function
-        method = '_eval_' + name.replace('-', '_')
-        if not hasattr(self, method):
-          raise UnsupportedError("The CSS content function %r is unsupported" % name)
-        method = getattr(self, method)
-        val = method(node, args)
-        if val is not None: vals.append(val)
-
-    if self.verbose: print >> sys.stderr, "DEBUG: Generated: [%s] from content:[%s]" % (''.join(vals), content)
-    ret = ''.join(vals)
-    return ret
-
-  def _eval_target_text(self, node, args):
-    (attr, which) = args
-    n = node
-    if self.is_pseudo(node):
-      n = node.getparent()
-    v = self.lookup_text(n, attr, which)
-    return v
-
-  def _eval_target_counter(self, node, args):
-    (attr, name, numbering) = args
-    n = node
-    if self.is_pseudo(node):
-      n = node.getparent()
-    v = self.lookup_counter(n, attr, name)
-    if v and name != 'page':
-      # TODO: use numbering to customize how it's rendered (decimal, upper-roman, etc)
-      return numbers.toString(v, numbering)
-
-  def _eval_counter(self, node, args):
-    # These look like: "counter(chapter)" or "counter(chapter, upper-roman)"
-    (name, numbering) = args
-    v = 0
-    if name in self.counters:
-      v = self.counters[name]
-    if v and name != 'page':
-      return numbers.toString(v, numbering)
-
-  def _eval_attr(self, node, args):
-    (name, type_, value) = args
-    n = node
-    if self.is_pseudo(node):
-      n = node.getparent()
-    v = n.attrib.get(name, '')
-    return v
-
-  def _eval_content(self, node, args):
-    # TODO: Just the text may not be enough to match the spec
-    assert args == ''
-    return node.text
-
-  def _eval_leader(self, node, args):
-    # Ignore the leader function
-    pass
-
-  # http://www.w3.org/TR/css3-gcpm/#using-named-strings
-  def _eval_string(self, node, args):
-    print "Evaluating string and it's: %s" % str(self.strings)
-    if args in self.strings:
-      return self.strings[args]
 
   def update_counters(self, node, d):
     if 'counter-reset' in d:
       for (name, v) in d['counter-reset']:
         if name == 'none': continue
         if self.verbose: print >> sys.stderr, "Resetting %s to %d" % (name, v)
-        self.counters[name] = v
+        self.evaluator.state.counters[name] = v
     if 'counter-increment' in d:
       for (name, v) in d['counter-increment']:
         if self.verbose: print >> sys.stderr, "Incrementing %s by %s" % (name, str(v))
-        if name not in self.counters:
-          self.counters[name] = 0
-        self.counters[name] += v
+        if name not in self.evaluator.state.counters:
+          self.evaluator.state.counters[name] = 0
+        self.evaluator.state.counters[name] += v
 
   def mutate_node(self, node):
     d = PropertyParser().parse(node.attrib.get(STYLE_ATTRIBUTE, ''))
@@ -234,7 +108,7 @@ class AddNumbering(object):
     # if there's a target-counter pointing to this node, squirrel the counter (TODO: Should this be done _before_ incrementing?)
     id = node.attrib.get('id', None)
     if id and id in self.node_at:
-      self.node_at[id] = State(node, self.counters, self.strings)
+      self.node_at[id] = State(node, self.evaluator.state)
     if d:
       # We'll have to look up the id later to find the counter
       if 'content' in d:
@@ -243,7 +117,7 @@ class AddNumbering(object):
           if key in [ 'target-counter', 'target-text' ]:
             has_target = True
         if has_target:
-          self.reprocess.append((node, State(node, self.counters, self.strings)))
+          self.reprocess.append((node, State(node, self.evaluator.state)))
         else:
           self._replace_content(node, d['content'])
       # http://www.w3.org/TR/css3-gcpm/#setting-named-strings-the-string-set-pro
@@ -253,14 +127,14 @@ class AddNumbering(object):
           if key in [ 'target-counter', 'target-text' ]:
             has_target = True
         if has_target:
-          self.reprocess.append((node, State(node, self.counters, self.strings)))
+          self.reprocess.append((node, State(node, self.evaluator.state)))
         else:
           string_name = d['string-set'][0]
           string_value = d['string-set'][1]
-          string_computed = self.make_content(node, string_value)
+          string_computed = self.evaluator.eval_content(node, string_value)
           # Note: The 1st "value" is actually the string name
           print "Setting string %s to [%s]" % (string_name, string_computed)
-          self.strings[string_name] = string_computed
+          self.evaluator.state.strings[string_name] = string_computed
 
   def expand_pseudo(self, node, style, class_ = ''):
     d = parse_style(style, class_)
